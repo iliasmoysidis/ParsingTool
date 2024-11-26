1f6e54bdb99b2d685376f5006509b1611564ccc2
Ilias <iliamous@iti.gr>
2024-11-26 13:32:43 +0200

    parse_datapoints returns a dictionary now

--- build/lib/parser/myfunctions.py
+++ build/lib/parser/myfunctions.py
@@ -16,26 +16,25 @@ def extract_values_and_timestamps(datapoints: list[dict]):
     for datapoint in datapoints:
         values.append(datapoint["datacellar:value"])
         timestamps.append(datapoint["datacellar:timeStamp"])
-    return values, timestamps
+    return {"values": values, "timestamps": timestamps}
 
-def parse_datapoints(fieldName:str, data: dict):
+def parse_datapoints(fieldName:str, data: dict) -> dict:
     """
-    Extracts the values and timestamps for a specific field name from a JSON data structure.
+    Extracts the values and timestamps for a specific field name from a JSON datacellar dataset.
 
     Parameters:
     ----------
     fieldName : str
         The name of the field to search for in the dataset (e.g., "outdoorTemperature").
     data : dict
-        The full dataset loaded from a JSON file. This must follow the expected structure,
+        The full datacellar dataset loaded from a JSON file. This must follow the expected structure,
         containing keys like 'datacellar:datasetSelfDescription' and 'datacellar:timeSeriesList'.
 
     Returns:
     -------
-    tuple[list[float], list[str]]
-        A tuple containing two lists:
-        - A list of values (floats) corresponding to the data points.
-        - A list of timestamps (strings) associated with each value.
+    Dict[str, Union[List[float], List[str]]]: A dictionary containing two keys:
+            - "values" (List[float]): A list of numerical values for the specified field.
+            - "timestamps" (List[str]): A list of corresponding timestamps for the values.
     """
     dataFieldsList = data["datacellar:datasetSelfDescription"]["datacellar:datasetFields"]
     timeSeriesList = data["datacellar:timeSeriesList"]
